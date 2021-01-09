e372095eab04652931709848c65a65d0d84bb4f6
sonquer <sonquer@o2.pl>
2021-01-09 15:24:40 +0100

    Added docker support and gateway api

---
+++ src/Services/Gateway/Gateway.Api/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace Gateway.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config
+                        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                        .AddJsonFile("appsettings.json", true, true)
+                        .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                        .AddJsonFile($"ocelot.json", false, true)
+                        .AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                        .AddEnvironmentVariables("APIGATEWAY_");
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ src/Services/Gateway/Gateway.Api/Startup.cs
@@ -0,0 +1,72 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace Gateway.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHealthChecks();
+            services.AddOcelot(Configuration);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwaggerUI(c => {
+                if (string.IsNullOrWhiteSpace(Configuration["BASEPATH"]) == false)
+                {
+                    c.RoutePrefix = Configuration["BASEPATH"];
+                }
+
+                var swaggerEndpoints = Configuration.GetSection("SwaggerEndpoints")
+                    .Get<Dictionary<string, string>>();
+
+                if (swaggerEndpoints is not null)
+                {
+                    foreach (var (name, url) in swaggerEndpoints)
+                    {
+                        c.SwaggerEndpoint(url, name);
+                    }
+                }
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseHealthChecks("/health", new HealthCheckOptions
+            {
+                AllowCachingResponses = false
+            });
+
+            app.UseOcelot().Wait();
+        }
+    }
+}
