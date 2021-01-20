ecfd4d523926df05482a2e7df3456763a6cc3b53
sonquer <sonquer@o2.pl>
2021-01-20 22:21:11 +0100

    Updated docker compose and create command

--- src/Services/Categories/Categories.Api/Application/Commands/CreateCategoryCommand.cs
+++ src/Services/Categories/Categories.Api/Application/Commands/CreateCategoryCommand.cs
@@ -5,8 +5,6 @@ namespace Categories.Api.Application.Commands
 {
     public class CreateCategoryCommand : IRequest<bool>
     {
-        public Guid Id { get; set; }
-
         public Guid? ParentId { get; set; }
 
         public string Name { get; set; }
