950521bac8b3e8d88a5f099c6cfc2c9c64032a70
kamilagruhlke <kgruhlke@gmail.com>
2021-01-30 17:38:12 +0100

    Added command and command handlers

---
+++ src/Services/Basket/Basket.Api/Application/Commands/SaveBasketCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+
+namespace Basket.Api.Application.Commands
+{
+    public class SaveBasketCommand : IRequest<Guid>
+    {
+        public Guid? BasketId { get; set; }
+
+        public IList<Guid> ProductIds { get; set; }
+    }
+}
