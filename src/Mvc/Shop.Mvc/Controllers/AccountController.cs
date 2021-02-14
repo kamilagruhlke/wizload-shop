172b4f542a73ed52ab4b195b030b9f1b161a5d45
sonquer <sonquer@o2.pl>
2021-02-14 19:25:25 +0100

    Added images lazy loading and login/logout in navbar

--- src/Mvc/Shop.Mvc/Controllers/AccountController.cs
+++ src/Mvc/Shop.Mvc/Controllers/AccountController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Shop.Mvc.Controllers
 {
+    [Authorize]
     public class AccountController : Controller
     {
         private readonly IHttpContextAccessor _httpContextAccesor;
@@ -13,12 +16,23 @@ namespace Shop.Mvc.Controllers
             _httpContextAccesor = httpContextAccesor;
         }
 
+        public IActionResult Login()
+        {
+            return Redirect(Url.Content("~/"));
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return Redirect(Url.Content("~/"));
+        }
+
+        [AllowAnonymous]
         public IActionResult AccessDenied(string returnUrl)
         {
             return View();
         }
 
-        [Authorize]
         public IActionResult Details()
         {
             return View(_httpContextAccesor);
