dd3e2574157d4dbf5ed22e4a2aa84645cf3019df
sonquer <sonquer@o2.pl>
2021-06-28 21:50:06 +0200

    Added simple product and categories loading

--- src/Spa/shop-spa/src/pages/home.tsx
+++ src/Spa/shop-spa/src/pages/home.tsx
@@ -1,28 +1,32 @@
-import { Box, Card, CardBody, Carousel, Image, Tab, Tabs, CardFooter } from 'grommet';
+import { Box, Card, CardBody, Carousel, Image, Tab, Tabs, CardFooter, Spinner } from 'grommet';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { API_GATEWAY } from '../configuration/url';
+import axios from 'axios';
 
-export default class Home extends React.Component {
+export default class Home extends React.Component<{}, {products: any, homePageIsLoading: boolean, categories: any}> {
     state = {
-      count: 0
+      products: [],
+      homePageIsLoading: true,
+      categories: []
     };
   
-    increment = () => {
-      this.setState({
-        count: (this.state.count + 1)
+    componentDidMount() {
+      axios.get(`${API_GATEWAY}/products/api/Products/Last/20`).then(res => {
+        const products = res.data;
+        this.setState({ products, homePageIsLoading: false });
       });
-    };
-  
-    decrement = () => {
-      this.setState({
-        count: (this.state.count - 1)
+
+      axios.get(`${API_GATEWAY}/categories/api/Categories/Active`).then(res => {
+        const categories = res.data;
+        this.setState({ categories });
       });
-    };
+    }
   
     render () {
       return (
         <div>
-          <Box height="medium" width="100%" overflow="hidden" background="light-6">
+          <Box height="large" width="100%" overflow="hidden" background="light-6">
             <Carousel fill controls="arrows">
               <Image fit="cover" src="img/carousel/1.jpg" />
               <Image fit="cover" src="img/carousel/2.jpg" />
@@ -31,42 +35,44 @@ export default class Home extends React.Component {
           </Box>
           <Box pad="medium">
             <Tabs>
-              <Tab title="Kategoria 1">
+              <Tab title="Strona główna">
                 <Box pad="medium" direction="row" gap="medium">
-                  <Link to="product/1" style={{ color: 'inherit', textDecoration: 'inherit'}}>
-                    <Card height="180px" width="180px">
-                      <CardBody>
-                        <Image src="img/carousel/1.jpg" fit="cover"/>
-                      </CardBody>
-                      <CardFooter pad="small">Nazwa produktu</CardFooter>
-                    </Card>
-                  </Link>
-
-                  <Link to="product/2" style={{ color: 'inherit', textDecoration: 'inherit'}}>
-                    <Card height="180px" width="180px">
-                      <CardBody>
-                        <Image src="img/carousel/2.jpg" fit="cover"/>
-                      </CardBody>
-                      <CardFooter pad="small">Nazwa produktu</CardFooter>
-                    </Card>
-                  </Link>
-
-                  <Link to="product/3" style={{ color: 'inherit', textDecoration: 'inherit'}}>
-                    <Card height="180px" width="180px">
-                      <CardBody>
-                        <Image src="img/carousel/3.jpg" fit="cover"/>
-                      </CardBody>
-                      <CardFooter pad="small">Nazwa produktu</CardFooter>
-                    </Card>
-                  </Link>
+                  {this.renderProducts()}
                 </Box>
               </Tab>
-              <Tab title="Kategoria 2">
-                <Box pad="medium">Kategoria 2</Box>
-              </Tab>
+              {this.renderCategories()}
             </Tabs>
           </Box>
         </div>
       );
     }
-  }
+
+    renderProducts = () => {
+      if (this.state.homePageIsLoading) {
+        return <div style={{margin:"0 auto"}}>
+          <Spinner size="medium" />
+        </div>;
+      }
+      
+      return this.state.products.map((product: any) => {
+        return <Link key={product.Id} to={`product/${product.Id}`} style={{ color: 'inherit', textDecoration: 'inherit'}}>
+          <Card height="180px" width="180px">
+            <CardBody>
+              <Image src="img/carousel/1.jpg" fit="cover"/>
+            </CardBody>
+            <CardFooter pad="small">Nazwa produktu</CardFooter>
+          </Card>
+        </Link>;
+      });
+    };
+
+    renderCategories = () => {
+      return this.state.categories.map((category: any) => {
+        return<Tab key={category.Id} title={category.Name}>
+          <Box pad="medium">
+            {category.Name}
+          </Box>
+        </Tab>;
+      });
+    }
+}
