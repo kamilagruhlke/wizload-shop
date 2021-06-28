bfaff82fb819e81190be85f3f27651be28cba4af
sonquer <sonquer@o2.pl>
2021-06-28 21:49:53 +0200

    Fixed dark mode (used localstorage)

--- src/Spa/shop-spa/src/App.tsx
+++ src/Spa/shop-spa/src/App.tsx
@@ -12,36 +12,51 @@ import Home from './pages/home';
 import Product from './pages/product';
 import NotFound from './pages/notFound';
  
-function App() {
-  const [darkMode, setDarkMode] = React.useState(false);
-  return (
-    <Router>
-      <Grommet full theme={theme as any} themeMode={darkMode ? "dark" : "light"}>
-        <Header>
-          <Box direction="row" gap="small" pad="small">
-            <Avatar src="//s.gravatar.com/avatar/00000000000000000000000000000000?s=80" />   
-            <Menu label="account" items={[{ label: 'logout' }]} />
-          </Box>
-          <Box pad="small">
-            <DarkModeToggle
-              onChange={() => setDarkMode(!darkMode)}
-              checked={darkMode}
-              size={48} />
-          </Box>
-        </Header>
+export default class App extends React.Component<{}, {darkMode: boolean}> {
+  constructor(props: any) {
+    super(props);
 
-        <Switch>
-          <Route exact path="/product/:id" render={({match}: any) => (<Product id={match.params.id} /> )} />
-          <Route exact path="/" component={Home} />
-          <Route component={NotFound} />
-        </Switch>
-        
-        <Footer pad="small">
-          <Text size="small">Copyright - WizLoad</Text>
-        </Footer>
-      </Grommet>
-    </Router>
-  );
+    if (localStorage.getItem('template_mode') == null) {
+      localStorage.setItem('template_mode', 'light');
+    }
+
+    this.state = {
+      darkMode: localStorage.getItem('template_mode') === 'dark' ? true : false
+    };
+  }
+
+  render () {
+  
+    return (
+      <Router>
+        <Grommet full theme={theme as any} themeMode={this.state.darkMode ? "dark" : "light"}>
+          <Header>
+            <Box direction="row" gap="small" pad="small">
+              <Avatar src="//s.gravatar.com/avatar/00000000000000000000000000000000?s=80" />   
+              <Menu label="account" items={[{ label: 'logout' }]} />
+            </Box>
+            <Box pad="small">
+              <DarkModeToggle
+                onChange={() => {
+                  localStorage.setItem('template_mode', !this.state.darkMode ? 'dark' : 'light');
+                  this.setState({ darkMode: !this.state.darkMode });
+                }}
+                checked={this.state.darkMode}
+                size={48} />
+            </Box>
+          </Header>
+  
+          <Switch>
+            <Route exact path="/product/:id" render={({match}: any) => (<Product id={match.params.id} /> )} />
+            <Route exact path="/" component={Home} />
+            <Route component={NotFound} />
+          </Switch>
+          
+          <Footer pad="small">
+            <Text size="small">Copyright - WizLoad</Text>
+          </Footer>
+        </Grommet>
+      </Router>
+    );
+  } 
 }
-
-export default App;
